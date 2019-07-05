eca62a8511d2dc3706915d9b0c40fe53960e4a89
Daniel <dan.hackl@gmail.com>
2019-07-05 14:52:45 -0400

    HashRouting was done improperly in main when it should have been index.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import LandingPage from "./landingPage";
 import About from "./aboutMe";
 import Contact from "./contact";
@@ -10,17 +10,13 @@ import Resume from "./resume";
 // but if not, you can do a function
 
 const Main = () => (
-  <HashRouter basename="/">
-    <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/resume" component={Resume} />
-      <Redirect from="/" to="/" />
-      <Redirect from="/personal-website/" to="/home" />
-    </Switch>
-  </HashRouter>
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={Projects} />
+    <Route path="/resume" component={Resume} />
+  </Switch>
 );
 
 export default Main;
