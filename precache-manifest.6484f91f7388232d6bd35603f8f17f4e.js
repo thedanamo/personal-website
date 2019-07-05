205720d4273c74c219e2601796f0f8f62d80047f
Daniel <dan.hackl@gmail.com>
2019-07-05 12:47:41 -0400

    Updates

---
+++ precache-manifest.6484f91f7388232d6bd35603f8f17f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa662790eeb3730b6d66832e7727f3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42ab190eb886614f2455",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "fd1d46170e3e87dfb07c",
+    "url": "/static/css/main.ac850948.chunk.css"
+  },
+  {
+    "revision": "42ab190eb886614f2455",
+    "url": "/static/js/2.813dc205.chunk.js"
+  },
+  {
+    "revision": "fd1d46170e3e87dfb07c",
+    "url": "/static/js/main.bb210e07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
