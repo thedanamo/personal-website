5abd5923e8aa852230d8cf49971e90300f0be248
Daniel <dan.hackl@gmail.com>
2019-07-06 15:21:49 -0400

    Respose to import warning. Removing BrowserRouter import since it was replaced by HashRouter.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "react-mdl/extra/material.css";
 import "react-mdl/extra/material.js";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 ReactDOM.render(
   <HashRouter>
     <App />
