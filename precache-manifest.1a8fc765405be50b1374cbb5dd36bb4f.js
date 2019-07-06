4d74a8eff9777476f1092a0d463e74443ee32614
Daniel <dan.hackl@gmail.com>
2019-07-06 13:47:00 -0400

    Updates

--- precache-manifest.1a8fc765405be50b1374cbb5dd36bb4f.js
+++ precache-manifest.1a8fc765405be50b1374cbb5dd36bb4f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "88f1ebbfca505110b721930418c66615",
+    "revision": "923940cad67796667b7311ea93d48fcd",
     "url": "/personal-website/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/personal-website/static/css/2.04aae6c4.chunk.css"
   },
   {
-    "revision": "29fe25c812e923e7e3a3",
-    "url": "/personal-website/static/css/main.f38acfd7.chunk.css"
+    "revision": "b17c0c422d0004838708",
+    "url": "/personal-website/static/css/main.74a0c261.chunk.css"
   },
   {
     "revision": "16dae1c197947ff28cd4",
     "url": "/personal-website/static/js/2.e18fc201.chunk.js"
   },
   {
-    "revision": "29fe25c812e923e7e3a3",
-    "url": "/personal-website/static/js/main.4b814473.chunk.js"
+    "revision": "b17c0c422d0004838708",
+    "url": "/personal-website/static/js/main.4105211c.chunk.js"
   },
   {
     "revision": "b6d8e3a721968ec40bad",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.83d6daca7bfa1efff52f57b662bcbd88.js"
+  "/personal-website/precache-manifest.1a8fc765405be50b1374cbb5dd36bb4f.js"
 );
 
 self.addEventListener('message', (event) => {
