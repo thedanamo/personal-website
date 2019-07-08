fbe6f149535ae1196cede737434ee981173cc748
Daniel <dan.hackl@gmail.com>
2019-07-08 14:51:55 -0400

    Updates

--- precache-manifest.5ea120b1ec7c3e3bccb3d44d72c5cfc9.js
+++ precache-manifest.5ea120b1ec7c3e3bccb3d44d72c5cfc9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c2b03a263945de723b4fa30c2101395c",
+    "revision": "ca3d94e5a2b831f9160f880201f5ce0c",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.04aae6c4.chunk.css"
   },
   {
-    "revision": "f20dca3608c585377485",
+    "revision": "fefcdbd160a0a6a61e26",
     "url": "/static/css/main.b942aca2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.45573b5e.chunk.js"
   },
   {
-    "revision": "f20dca3608c585377485",
-    "url": "/static/js/main.caf0bce0.chunk.js"
+    "revision": "fefcdbd160a0a6a61e26",
+    "url": "/static/js/main.0dc87c08.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e752314d67a616abf96ae0560383633.js"
+  "/precache-manifest.5ea120b1ec7c3e3bccb3d44d72c5cfc9.js"
 );
 
 self.addEventListener('message', (event) => {
